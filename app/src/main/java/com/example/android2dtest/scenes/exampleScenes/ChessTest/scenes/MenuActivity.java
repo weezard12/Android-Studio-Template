ada66f2f53278c95f495e04f4f878527638a8cdd
weezard12 <weezard1234@gmail.com>
2025-02-23 23:25:48 +0200

    started adding on click

--- app/src/main/java/com/example/android2dtest/scenes/exampleScenes/ChessTest/scenes/MenuActivity.java
+++ app/src/main/java/com/example/android2dtest/scenes/exampleScenes/ChessTest/scenes/MenuActivity.java
@@ -1,6 +1,9 @@
 package com.example.android2dtest.scenes.exampleScenes.ChessTest.scenes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +13,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.android2dtest.R;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton logoButton;
+    Button hotSitButton;
+    Button botButton;
+    Button settingsButton;
+    Button quitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,31 @@ public class MenuActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        logoButton = (ImageButton) findViewById(R.id.logo_image_button);
+        hotSitButton = (Button) findViewById(R.id.btn_play_hot_seat);
+        botButton = (Button) findViewById(R.id.btn_play_vs_bot);
+        settingsButton = (Button) findViewById(R.id.btn_settings);
+        quitButton = (Button) findViewById(R.id.btn_quit);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(logoButton.equals(v)){
+
+        }
+        else if(hotSitButton.equals(v)){
+
+        }
+        else if(botButton.equals(v)){
+
+        }
+        else if(settingsButton.equals(v)){
+
+        }
+        else if(quitButton.equals(v)){
+
+        }
     }
 }
