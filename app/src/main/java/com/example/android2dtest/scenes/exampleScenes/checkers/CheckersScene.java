85708834b030baae82a4123d1acfa2fa5a480c00
weezard12 <weezard1234@gmail.com>
2025-03-28 20:40:19 +0300

    Started making a checkers game added board with tiles

--- app/src/main/java/com/example/android2dtest/scenes/exampleScenes/checkers/CheckersScene.java
+++ app/src/main/java/com/example/android2dtest/scenes/exampleScenes/checkers/CheckersScene.java
@@ -11,10 +11,10 @@ import com.example.android2dtest.gameLogic.myPhysics.PhysicsSystem;
 
 public class CheckersScene extends GameScene {
 
+    private GridEntities entityGrid;
+
     public CheckersScene(Context context) {
         super(context);
-
-
     }
 
     @Override
@@ -24,20 +24,45 @@ public class CheckersScene extends GameScene {
         debugRenderPhysics = true;
         debugRenderScene = true;
 
-        GridEntities entityGrid = new GridEntities(8, 8, 110, 110);
-        entityGrid.setupBoardWithNewEntities();
-        for (GameEntity[] row : entityGrid.getGrid()) {
-            for (GameEntity e : row)
-                addEntity(e);
+        entityGrid = new GridEntities(8, 8, 110, 110);
+
+        for (int y = 0; y < entityGrid.getGrid().length; y++) {
+            for (int x = 0; x < entityGrid.getGrid()[y].length; x++) {
+                entityGrid.getGrid()[y][x] = new CheckersTile(x, y, entityGrid);
+                addEntity(entityGrid.getGrid()[y][x]);
+            }
         }
+
         entityGrid.setPosition(getSurfaceCenter().x, getSurfaceCenter().y);
         entityGrid.setGridClickable();
+
+        setupBoardFromString(
+                " b b b b" +
+                "b b b b " +
+                " b b b b" +
+                "        " +
+                "        " +
+                "w w w w " +
+                " w w w w" +
+                "w w w w "
+        );
 
         entityGrid.setOnClickListener(new GridEntities.OnClickListener() {
             @Override
             public void onClick(int row, int column) {
-                Log.i("Board","Clicked: " + row + " " + column);
+                Log.i("Board", "Clicked: " + row + " " + column);
             }
         });
     }
+
+    private void setupBoardFromString(String boardString) {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                char tileTypeChar = boardString.charAt(x + (y * 8));
+                CheckersTile tile = (CheckersTile) entityGrid.getGrid()[y][x];
+                tile.setTileType(TileType.fromChar(tileTypeChar));
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/android2dtest/scenes/exampleScenes/checkers/CheckersTile.java
@@ -0,0 +1,76 @@
+package com.example.android2dtest.scenes.exampleScenes.checkers;
+
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.PointF;
+
+import com.example.android2dtest.gameLogic.math.GridEntities;
+import com.example.android2dtest.gameLogic.myECS.GameScene;
+import com.example.android2dtest.gameLogic.myECS.components.renderable.BoxRenderer;
+import com.example.android2dtest.gameLogic.myECS.components.renderable.CircleRenderer;
+import com.example.android2dtest.gameLogic.myECS.entities.GameEntity;
+
+public class CheckersTile extends GameEntity {
+
+    private final Point positionOnBoard;
+    private final GridEntities board;
+    private TileType type;
+
+    private CircleRenderer pieceRenderer;
+
+    public CheckersTile(int x, int y, GridEntities gridEntities) {
+        this.positionOnBoard = new Point(x,y);
+        this.board = gridEntities;
+        type = TileType.EMPTY;
+    }
+
+    @Override
+    public void attachToScene(GameScene scene) {
+        super.attachToScene(scene);
+
+        setupComponents();
+    }
+
+    private void setupComponents() {
+        // setup background color
+        BoxRenderer tileColor = new BoxRenderer(board.getRowDistance());
+
+        // Determine the color based on the checkerboard pattern
+        if ((positionOnBoard.x + positionOnBoard.y) % 2 == 0) {
+            tileColor.paint.setColor(Color.WHITE); // White tile
+        } else {
+            tileColor.paint.setColor(Color.GRAY); // Black tile (or any other color)
+        }
+
+        addComponent(tileColor);
+
+        pieceRenderer = new CircleRenderer((int)(board.getRowDistance() / 2.4));
+        pieceRenderer.paint.setColor(Color.RED);
+
+        pieceRenderer.setEnabled(false);
+
+        addComponent(pieceRenderer);
+    }
+
+    public void setTileType(TileType type){
+        this.type = type;
+
+        pieceRenderer.setEnabled(false);
+
+        switch (type){
+            case EMPTY:
+                break;
+
+            case BLACK:
+                pieceRenderer.paint.setColor(Color.BLACK);
+                pieceRenderer.setEnabled(true);
+                break;
+
+            case WHITE:
+                pieceRenderer.paint.setColor(Color.WHITE);
+                pieceRenderer.setEnabled(true);
+                break;
+        }
+
+    }
+}
