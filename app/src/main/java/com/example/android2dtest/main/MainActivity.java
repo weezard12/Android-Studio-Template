5e19fc74b4464857d364dec5a819e1f3bb983e5d
weezard12 <weezard1234@gmail.com>
2025-04-16 18:11:45 +0300

    Added sprite animation

--- app/src/main/java/com/example/android2dtest/main/MainActivity.java
+++ app/src/main/java/com/example/android2dtest/main/MainActivity.java
@@ -16,6 +16,7 @@ import com.example.android2dtest.gameLogic.myPhysics.PhysicsSystem;
 import com.example.android2dtest.scenes.exampleScenes.ChessTest.scenes.LoadingScreenActivity;
 import com.example.android2dtest.scenes.exampleScenes.ChessTest.scenes.MenuActivity;
 import com.example.android2dtest.scenes.exampleScenes.ChessTest.scenes.base.ChessHotSit;
+import com.example.android2dtest.scenes.exampleScenes.RenderingExample;
 import com.example.android2dtest.scenes.exampleScenes.SwordThrow;
 import com.example.android2dtest.scenes.exampleScenes.checkers.CheckersScene;
 
@@ -29,21 +30,21 @@ public class MainActivity extends AppCompatActivity {
         //GameScene scene = new FlappyBirdScene(getApplicationContext());
         //GameScene scene = new PhysicsExampleScene(getApplicationContext());
         //GameScene scene = new TicTacToe(getApplicationContext());
-        //GameScene scene = new RenderingExample(getApplicationContext());
+        GameScene scene = new RenderingExample(getApplicationContext());
         //GameScene scene = new MemoryGame1Player(getApplicationContext());
         //GameScene scene = new MemoryGame2Players(getApplicationContext());
         //GameScene scene = new BattleShipWar(getApplicationContext());
         //GameScene scene = new SwordThrow(getApplicationContext());
-        GameScene scene = new CheckersScene(getApplicationContext());
+        //GameScene scene = new CheckersScene(getApplicationContext());
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         // Request window without title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         // Hide the status bar
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                //WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         /*GameScene scene = new ChessHotSit(getApplicationContext());
         scene.setFullScreen();*/
--- app/src/main/java/com/example/android2dtest/scenes/exampleScenes/RenderingExample.java
+++ app/src/main/java/com/example/android2dtest/scenes/exampleScenes/RenderingExample.java
@@ -3,7 +3,10 @@ package com.example.android2dtest.scenes.exampleScenes;
 import android.content.Context;
 
 import com.example.android2dtest.R;
+import com.example.android2dtest.gameLogic.ContentManager;
 import com.example.android2dtest.gameLogic.GameLoop;
+import com.example.android2dtest.gameLogic.myECS.components.renderable.SpriteAnimation;
+import com.example.android2dtest.gameLogic.myECS.components.renderable.SpriteAnimationRenderer;
 import com.example.android2dtest.gameLogic.myECS.components.touchable.DraggableComponent;
 import com.example.android2dtest.gameLogic.myECS.GameScene;
 import com.example.android2dtest.gameLogic.myECS.components.renderable.RotatableSpriteRenderer;
@@ -14,6 +17,7 @@ import com.example.android2dtest.gameLogic.myPhysics.BoxCollider;
 public class RenderingExample extends GameScene {
 
     private GameEntity textureEntity;
+    private GameEntity animationEntity;
 
     /**
      * WARNING when inheriting the class do not use the constructor scene init logic!
@@ -43,9 +47,19 @@ public class RenderingExample extends GameScene {
         textureEntity.addComponent(new DraggableComponent(textureEntity.getComponent(BoxCollider.class)));
         textureEntity.addComponent(renderer);
 
-        //textureEntity.getTransform().rotation = 180;
 
+        SpriteAnimationRenderer animationRenderer = new SpriteAnimationRenderer();
+        animationRenderer.setScale(10);
+        animationRenderer.addAnimation("attack",new SpriteAnimation(ContentManager.loadSpriteSheetFrames(getContext(),R.drawable.solider_attack1,1,6,6),6, SpriteAnimation.LoopMode.LOOP));
 
+        animationEntity = new GameEntity("animation");
+        addEntity(animationEntity);
+
+        animationEntity.setPosition(getSurfaceCenter());
+        animationEntity.addComponent(new BoxCollider(100,100));
+        animationEntity.addComponent(new DraggableComponent(textureEntity.getComponent(BoxCollider.class)));
+        animationEntity.addComponent(animationRenderer);
+        animationRenderer.playAnimation("attack");
     }
 
     @Override
